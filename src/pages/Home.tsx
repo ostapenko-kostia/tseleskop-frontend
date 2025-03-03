0bf919ddd37ef8535ade691fd545f64caa6d52de
Ostapenko Kostiantyn <ostapenkokpersonal@gmail.com>
2025-03-03 18:57:34 +0200

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router'
 import { HomeCreateGoal } from '../components/home/home-create-goal'
 import { HomeList } from '../components/home/home-list'
 import { HomeStatistics } from '../components/home/home-statistics'
@@ -10,6 +11,7 @@ export function HomePage() {
 				background: 'linear-gradient(180deg, #FFFFFF 65.62%, #4982F6 100%)',
 			}}
 		>
+			<Link to='/register'>На страницу регистрации</Link>
 			<HomeStatistics />
 			<HomeList />
 			<HomeCreateGoal className='fixed bottom-5 right-5 [&_button]:aspect-square [&_button]:p-0 [&_button]:w-16 text-4xl [&_button]:h-16 [&_button]:flex [&_button]:items-center [&_button]:justify-center' />
