df4317c4d3a43e19aa9a7ba75a803a19bc64562e
Ostapenko Kostiantyn <ostapenkokpersonal@gmail.com>
2025-03-06 11:58:33 +0200

    fix

--- src/utils/parseInitData.ts
+++ src/utils/parseInitData.ts
@@ -1,21 +1,21 @@
 export function parseInitData(query: string) {
-	const params = new URLSearchParams(query);
-	const user = JSON.parse(decodeURIComponent(params.get("user") || "{}"));
+	const params = new URLSearchParams(query)
+	const user = JSON.parse(decodeURIComponent(params.get('user') || '{}'))
 
 	return {
-		auth_date: params.get("auth_date") || "",
-		chat_instance: params.get("chat_instance") || "",
-		chat_type: params.get("chat_type") || "",
-		hash: params.get("hash") || "",
-		signature: params.get("signature") || "",
+		auth_date: new Date(params.get('auth_date') || ''),
+		chat_instance: params.get('chat_instance') || '',
+		chat_type: params.get('chat_type') || '',
+		hash: params.get('hash') || '',
+		signature: params.get('signature') || '',
 		user: {
 			id: user.id || 0,
-			first_name: user.first_name || "",
-			last_name: user.last_name || "",
-			username: user.username || "",
-			language_code: user.language_code || "",
+			first_name: user.first_name || '',
+			last_name: user.last_name || '',
+			username: user.username || '',
+			language_code: user.language_code || '',
 			allows_write_to_pm: user.allows_write_to_pm || false,
-			photo_url: user.photo_url || "",
+			photo_url: user.photo_url || '',
 		},
-	};
+	}
 }
