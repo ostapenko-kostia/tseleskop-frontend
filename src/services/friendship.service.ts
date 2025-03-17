d7a6bd5a1509151219f398ab3354e058632bf1d7
Ostapenko Kostiantyn <ostapenkokpersonal@gmail.com>
2025-03-17 10:50:47 +0200

    fix

--- src/services/friendship.service.ts
+++ src/services/friendship.service.ts
@@ -2,8 +2,8 @@ import toast from 'react-hot-toast'
 import { api } from '../lib/axios'
 
 class FriendshipService {
-	async createFriendship(inviteId: string) {
-		const res = await api.post('/friendship/add', { inviteId })
+	async createFriendship(friendId: string) {
+		const res = await api.post('/friendship/add', { friendId })
 		if (res.status === 200) toast.success('Друг успешно добавлен!')
 		return res
 	}
